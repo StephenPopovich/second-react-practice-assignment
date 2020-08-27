6d21ec319c65f565716750866b3f85195516d25a
StephenPopovich <stephenpopovich16@gmail.com>
2020-08-27 14:31:43 -0700

    create an app template with assignment instructions

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// Assignment Instructions:
+// 1. Create an input field (in App component) with a change listener which outputs the length of the entered text below (e.g. in a pargraph).
+// 2. Create a new component (=> ValidationComponent) which receives the text length as a prop
+// 3. Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)
+// 4. Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black)
+// 5. Render a list of CharComponent where each CharComponent receives a different eltter of the entereed test (in the initial input fild) as a prop.
+// 6. When you click a CharComponent, it should be removed from the enetered text.
+//Hint: Keep in mind that JavaScript strings are basically arrays!
+
+
+// *** LAST LEFT ON TIME STAMP , # ****
+
+
+
+  render() {
+    return (
+      <div className="App">
+            <h1 className="App-header"><u>Second React Practice Assignment Instructions:</u></h1>
+          <ol>
+            <li>1. Create an input field (in App component) with a change listener which outputs the length of the entered text below (e.g. in a pargraph).</li>
+            <li>2. Create a new component (=> ValidationComponent) which receives the text length as a prop.</li>
+            <li>3. Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length).</li>
+            <li>4. Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
+            <li>5. Render a list of CharComponent where each CharComponent receives a different eltter of the entereed test (in the initial input fild) as a prop.</li>
+            <li>6. When you click a CharComponent, it should be removed from the enetered text.</li>
+            <p><i>Hint: Keep in mind that JavaScript strings are basically arrays!</i></p>
+
+          </ol>
+
+      </div>
+    );
+  }
+}
+
+export default App;

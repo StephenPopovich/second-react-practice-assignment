cecd5d1b59270e209b12ea2172c37c908d89b199
StephenPopovich <stephenpopovich16@gmail.com>
2020-08-29 17:12:02 -0700

    create deleteCharHandler method that triggers onClick function that deletes selected characters from page and input field

--- src/App.js
+++ src/App.js
@@ -23,18 +23,29 @@ class App extends Component {
    }
 
   // a method to change input data passing an event object and stored in state
-
    inputChangeHandler = (event) => {
      this.setState({userInput: event.target.value});
    }
 
 
+ // a method When you click a CharComponent, it should be removed and removed from the enetered text.
+  deleteCharHandler = (index) => {
+      const text = this.state.userInput.split('');
+      text.splice(index,1);
+      const updatedText = text.join('');
+      this.setState({userInput: updatedText});
+  }
+
+
   render() {
   // a constant variable that reaches out to the state of the userInput that maps every elemenet of this array into a new element
  // map() doesnt touch the original array it simply gives you a new array that stored in charList
- // String are bascially JavaScript arrays but not really to turn them into arrays call split() on this will basically slips them in array of strings after every every character
+ // String are kinda of JavaScript arrays but not really to turn them into arrays call split() on this will basically slips them in array of strings after every every character
   const charList = this.state.userInput.split('').map((char, index) => {
-    return <Char character={char} key={index} />;
+    return <Char
+      character={char}
+      key={index}
+      clicked={() => this.deleteCharHandler(index)}/>;
   });
 
 
@@ -46,7 +57,7 @@ class App extends Component {
                 <li>Create a new component (=> ValidationComponent) which receives the text length as a prop.</li>
                 <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length).</li>
                 <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
-                <li>Render a list of CharComponent where each CharComponent receives a different eltter of the entereed test (in the initial input fild) as a prop.</li>
+                <li> Render a list of CharComponent where each CharComponent receives a different letter of the entered text (in the initial input fild) as a prop.</li>
                 <li>When you click a CharComponent, it should be removed from the enetered text.</li>
                 <p><i>Hint: Keep in mind that JavaScript strings are basically arrays!</i></p>
               </ol>
@@ -63,7 +74,8 @@ class App extends Component {
 
         <p><i>Pass validation component as a self closing tag and passing the text length as a prop.</i></p>
         <Validation inputLength={this.state.userInput.length}/>
-        <p><i>Pass another component (=> CharComponent) and style it as an inline box:</i></p>
+        <p><i>Pass another component (=> CharComponent) and style it as an inline box.</i></p>
+        <p><i>Render a list of CharComponent where each CharComponent receives a different letter of the entered text (in the initial input fild) as a prop.</i></p>
          {charList}
 
 
